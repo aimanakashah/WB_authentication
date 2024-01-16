49b1627f58b9a067e45fe5746f16029d2aec29fc
aimanakashah <aimanakashah@gmail.com>
2024-01-17 02:01:14 +0800

    Level 6: OAuth: Implement Sign In with Google

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import bcrypt, { hash } from "bcrypt";
 import session from "express-session";
 import passport from "passport";
 import { Strategy } from "passport-local";
+import GoogleStrategy from "passport-google-oauth2";
 import env from "dotenv";
 
 const app = express();
@@ -12,8 +13,6 @@ const port = 3000;
 const saltRounds = 10;
 env.config();
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
 app.use(
   session({
     secret: process.env.SESSION_KEY,
@@ -25,6 +24,8 @@ app.use(
     },
   })
 );
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -42,6 +43,10 @@ app.get("/", (req, res) => {
   res.render("home.ejs");
 });
 
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
 app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
@@ -53,9 +58,36 @@ app.get("/secrets", (req, res) => {
     res.redirect("/login");
   }
 });
-app.get("/login", (req, res) => {
-  res.render("login.ejs");
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+
+app.get(
+  "/auth/google/secrets",
+  passport.authenticate("google", {
+    successRedirect: "/secrets",
+    failureRedirect: "/login",
+  })
+);
+
+app.get("/logout", (req, res) => {
+  req.logout((err) => {
+    if (err) console.log(err);
+    res.redirect("/");
+  });
 });
+
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/secrets",
+    failureRedirect: "/login",
+  })
+);
 
 app.post("/register", async (req, res) => {
   const email = req.body.username;
@@ -67,11 +99,12 @@ app.post("/register", async (req, res) => {
     ]);
     //using the length is because the the data is already extracted from database. if no data extracted meaning the length is 0 thus the email does not exist yet
     if (checkResult.rows.length > 0) {
-      res.send("Email already exists, try logging in.");
+      req.redirect("/login");
+      // res.send("Email already exists, try logging in.");
     } else {
       bcrypt.hash(password, saltRounds, async (err, hash) => {
         if (err) {
-          console.log("Error hashing password", err);
+          console.error("Error hashing password", err);
         } else {
           const result = await db.query(
             "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *;",
@@ -91,15 +124,8 @@ app.post("/register", async (req, res) => {
   }
 });
 
-app.post(
-  "/login",
-  passport.authenticate("local", {
-    successRedirect: "/secrets",
-    failureRedirect: "/login",
-  })
-);
-
 passport.use(
+  "local",
   new Strategy(async function verify(username, password, cb) {
     //the 'verify' function get the data like 'username' and 'password' straight from ejs name as long as it is the same, thus no need to extract it like using bodyParser
     try {
@@ -132,6 +158,39 @@ passport.use(
       return cb(err);
     }
   })
+);
+
+passport.use(
+  "google",
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/secrets",
+      userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+    },
+    async (accessToken, refreshToken, profile, cb) => {
+      console.log(profile);
+      try {
+        const result = await db.query("SELECT * FROM users WHERE email = $1", [
+          profile.email,
+        ]);
+
+        if (result.rows.length === 0) {
+          const newUser = await db.query(
+            "INSERT INTO users (email, password) VALUES ($1, $2)",
+            [profile.email, "google"]
+          );
+          return cb(null, newUser.rows[0]);
+        } else {
+          //Already have user, passing result since it is in different part of if else
+          return cb(null, result.rows[0]);
+        }
+      } catch (err) {
+        cb(err);
+      }
+    }
+  )
 );
 
 //the function called when a user is authenticated, its purpose is to determine which data of the user object should be stored in the session
